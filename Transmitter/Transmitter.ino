b16b4340569015b514ce1b971ccea2c97fbfc842
Evan Giarta <Evan@dn0a2255fd.sunet>
2015-04-10 14:10:04 -0700

    Added functions to accept user command input and print GSM shield response.

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -27,17 +27,22 @@
 
 SoftwareSerial shieldGSM(7,8);
 
+boolean flagSendSMS;
+
 void setup() {
   Serial.begin(19200);
   //shieldGSM = new SoftwareSerial(7,8);
   shieldGSM.begin(19200);    // the GPRS baud rate
   Serial.println("ESW RMS Transmitter initializing...");
+  flagSendSMS = false;
   delay(500);
   Serial.println("Initialization complete!");
   TMRArd_InitTimer(0, PRINT_TIME);
 }
 
 void loop() {
+  pollUserCommand();
+  printShieldGSMResponse();
 }
 
 void printSensorSample(){
@@ -55,4 +60,30 @@ void printSensorSample(){
     Serial.println();
     TMRArd_InitTimer(0, PRINT_TIME);
   }
+}
+
+void pollUserCommand() {
+  char avail = Serial.available();
+  if (avail) {
+    String buffer = Serial.readString();
+    buffer.trim();
+    if (!flagSendSMS) buffer.replace(" ","");
+    Serial.println(buffer);
+    shieldGSM.println(buffer);
+    delay(100);
+    
+    if (flagSendSMS) {
+      shieldGSM.println((char)26);
+      delay(100);
+      flagSendSMS = false;
+    }
+    
+    if (buffer.startsWith("AT+CMGS")) flagSendSMS = true;
+    
+  }
+}
+
+void printShieldGSMResponse() {
+  while (shieldGSM.available()) Serial.write(shieldGSM.read());
+  Serial.flush();
 }
