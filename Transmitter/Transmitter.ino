572e2f44e3630332339c28f95d992970ea0398f2
0evelyn <0evelynli@gmail.com>
2015-05-08 16:58:42 -0700

    Set up automated SMS message sending in loop. Issues with getting sensor data while interfacing with GSM shield prevent printing out of updated data values. Currently testing free running mode method.

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -9,13 +9,14 @@
  * Authors: Evan Giarta, egiarta1671
  *          Evelyn Li, 0evelyn
  *
- * Version: V1, 2015.04.01
- * Maiden code to test Arduino's sampling capability
- * off the ESW Three Phase Sensor Board v2 with minimal
- * Geetech SIM 900 GPRS GSM Shield interfacing.
+ * Version: V2, 2015.05.08
  * 
  * Timer Interrupt code modified from http://www.instructables.com/id/Arduino-Timer-Interrupts/?ALLSTEPS
- *
+ * Free Running Mode Test code from http://www.glennsweeney.com/tutorials/interrupt-driven-analog-conversion-with-an-atmega328p
+ * ATMega328p
+ * Note, many macro values are defined in <avr/io.h> and
+ * <avr/interrupts.h>, which are included automatically by
+ * the Arduino interface
  */
 
 #include <SoftwareSerial.h>
@@ -35,7 +36,7 @@
 #define ANALOG_PIN_OFFSET 3
 #define PHASE_NUMBER_OFFSET 13
 #define OUTPUT_PIN 13
-#define SMS_SEND_PERIOD 60                                    // in seconds, this will be 3600 = 1 hour
+#define SMS_SEND_PERIOD 32                                    // in seconds, this will be 3600 = 1 hour
 #define INTERRUPT_PERIOD 4 // highest integer numbers of second a timer interrupt is achievable with 16MHz clock and 1024 pre scale factor
 #define SMS_INTERRUPT_CYCLES SMS_SEND_PERIOD/INTERRUPT_PERIOD // remove this when testing is done
 #define NUM_SMS_COMMANDS 4
@@ -43,6 +44,12 @@
 
 SoftwareSerial shieldGSM(7,8);
 boolean flagSendSMS;
+boolean flagAutoSMS;
+// High when a value is ready to be read
+volatile int readFlag;
+
+// Value to store analog result
+volatile int analogVal;
 
 struct ATcommand {
   String cmd;
@@ -66,11 +73,13 @@ quantity *sensorInputs[6] = {&v1, &v2, &v3, &i1, &i2, &i3};
 
 void setup() {
   //powerUp();
+//  cli();
   Serial.begin(BAUD_RATE);
   shieldGSM.begin(BAUD_RATE);    // the GPRS baud rate
   Serial.println("ESW RMS Transmitter initializing...");
-  synchronizeLocalTime();
+//  synchronizeLocalTime();
   initializeTimerInterrupts();
+  ADCsetup();
   
   v1.port = A3;
   v2.port = A4;
@@ -78,20 +87,63 @@ void setup() {
   i1.port = A0;
   i2.port = A1;
   i3.port = A2;
-
+    
   Serial.println("Initialization complete!");
   // TMRArd_InitTimer(0, PRINT_TIME);
+//  sei();
 }
 
-void loop() {
+//void loop() {
 //  pollUserCommand();
-  //printShieldGSMResponse("");
-  if(Serial.available()) {
-    Serial.println(millis());
-    Serial.read();
+//    
+//  while (shieldGSM.available()) Serial.write(shieldGSM.read());
+//
+//  if(flagAutoSMS) {
+////    sendSMSMessage("message from loop");
+////    Serial.println("message from loop");
+//    flagAutoSMS=false;  
+//  }
+//
+//  for(register int i=0;i<1;i++) {
+//    quantity *q = sensorInputs[0];
+//    unsigned int sensorReading = analogRead(q->port);
+//    if (sensorReading > q->max) {
+//      q->max = sensorReading;
+//    }
+//    if (sensorReading < q->min) {
+//      q->min = sensorReading;
+//    }
+//  }
+//}
+
+// Processor loop
+void loop(){
+
+  // Check to see if the value has been updated
+  if (readFlag == 1){
+    
+    Serial.println(analogVal);
+    // Perform whatever updating needed
+    
+    readFlag = 0;
   }
   
-  while (shieldGSM.available()) Serial.write(shieldGSM.read());
+  // Whatever else you would normally have running in loop().
+  
+}
+
+// Interrupt service routine for the ADC completion
+ISR(ADC_vect){
+  
+  // Done reading
+  readFlag = 1;
+  
+  // Must read low first
+  analogVal = ADCL | (ADCH << 8);
+  
+  // Not needed because free-running mode is enabled.
+  // Set ADSC in ADCSRA (0x7A) to start another ADC conversion
+  // ADCSRA |= B01000000;
 }
 
 boolean toggle2;
@@ -99,32 +151,34 @@ unsigned int v1sample = 0;
 unsigned int v1sample_prev = 1024;
 unsigned int v1max = 0;
 unsigned int v1min = 1024;
+int sensorval=0;
 ISR(TIMER2_COMPA_vect){//timer0 interrupt 2kHz toggles pin 8    
-//  for(register int i=0;i<6;i++) {
-//    quantity *q = sensorInputs[i];
-//    if (analogRead(q->port) > q->max) {
-//      q->max = analogRead(q->port);
+//  for(register int i=0;i<1;i++) {
+//    quantity *q = sensorInputs[0];
+//    unsigned long sensorReading = analogRead(q->port);
+//    if (sensorReading > q->max) {
+//      q->max = sensorReading;
 //    }
-//    if (analogRead(q->port) < q->min) {
-//      q->min = analogRead(q->port);
+//    if (sensorReading < q->min) {
+//      q->min = sensorReading;
 //    }
 //  }
-  
-//  if ((v1sample > 511) && (v1sample_prev < 511)) {
-//    v1.freq = micros() - v1.mrrz;
-//    v1.mrrz = micros();
-//  }
-//  v1sample_prev = v1sample;
+    
+  if ((v1sample > 511) && (v1sample_prev < 511)) {
+    v1.freq = micros() - v1.mrrz;
+    v1.mrrz = micros();
+  }
+  v1sample_prev = v1sample;
 
 //generates pulse wave of frequency interrupt/2 (takes two cycles for full wave- toggle high then toggle low)
-  if (toggle2){
-    digitalWrite(8,HIGH);
-    toggle2 = 0;
-  }
-  else{
-    digitalWrite(8,LOW);
-    toggle2 = 1;
-  }
+//  if (toggle2){
+//    digitalWrite(13,HIGH);
+//    toggle2 = 0;
+//  }
+//  else{
+//    digitalWrite(13,LOW);
+//    toggle2 = 1;
+//  }
 }
 
 char count = 0;
@@ -149,7 +203,8 @@ ISR (TIMER1_COMPA_vect) { // timer one interrupt function
 //    }
 
     //sendSMSMessage("timer interrupt message");
-    Serial.println("timer interrupt message");
+    flagAutoSMS=true;
+//    Serial.println("timer interrupt message");
     count = 0;
   }
 }
@@ -166,6 +221,7 @@ void sensorDataMessage(int i) { //change to String if using dataMessage
 void executeATCommands(struct ATcommand *commandsList, int numCommands) {
     for (int i = 0; i < numCommands; i++) {
       struct ATcommand atc = commandsList[i];
+      int tocount=0;
       int state = INIT_WAIT_CODE; 
       TMRArd_InitTimer(0, INIT_TIME);
       Serial.print(i);
@@ -181,13 +237,14 @@ void executeATCommands(struct ATcommand *commandsList, int numCommands) {
         }
         switch(state) {
           case INIT_TIMEOUT_CODE:
-            Serial.println("COMMAND TIMEOUT");  // intentionally no break
+            Serial.println("COMMAND TIMEOUT: "+String(++tocount));  // intentionally no break
           case INIT_ERROR_CODE:
-            executeUserCommand("A/");
+            executeUserCommand(atc.cmd);
             break;
           default:
             break;
         }   
+        if(tocount>=3)return;
       } while(state != INIT_SUCCESS_CODE);
     }
 }
@@ -221,21 +278,23 @@ void executeUserCommand(String buffer) {
 void pollUserCommand() {
   char avail = Serial.available();
   if (avail) {
+    Serial.println(millis());
     String buffer = Serial.readString();
-    buffer.trim();
-    if (!flagSendSMS) buffer.replace(" ","");
-    Serial.println(buffer);
-    shieldGSM.println(buffer);
-    delay(100);
-    
-    if (flagSendSMS) {
-      shieldGSM.println((char)26);
-      delay(100);
-      flagSendSMS = false;
-    }
-    
-    if (buffer.startsWith("AT+CMGS")) flagSendSMS = true;
-    
+    executeUserCommand(buffer);
+//    buffer.trim();
+//    if (!flagSendSMS) buffer.replace(" ","");
+//    Serial.println(buffer);
+//    shieldGSM.println(buffer);
+//    delay(100);
+//    
+//    if (flagSendSMS) {
+//      shieldGSM.println((char)26);
+//      delay(100);
+//      flagSendSMS = false;
+//    }
+//    
+//    if (buffer.startsWith("AT+CMGS")) flagSendSMS = true;
+//    
   }
 }
 
@@ -285,7 +344,6 @@ void initializeTimerInterrupts() {
   TCCR1A = 0;   // clearing registers 
   TCCR1B = 0;
   TCNT1 = 0;
-  
   OCR1A = COMPARE_MATCH_REGISTER; // for 0.25 Hz (16e6)/(0.25*1024) - 1= clk_spd/(des_frq*pre_scale) - 1
   TCCR1B |= (1 << WGM12); // turn on CTC mode
   TCCR1B |= (1 << CS12) | (1 << CS10); // set 1024 prescale factor
@@ -301,6 +359,59 @@ void initializeTimerInterrupts() {
 
   sei();         // set enable interrupt reallow interrupts
   
-  pinMode(OUTPUT_PIN,OUTPUT); 
+//  pinMode(OUTPUT_PIN,OUTPUT); 
   Serial.println("Timer interrupts initialized.");
 }
+
+void ADCsetup() {  
+  // clear ADLAR in ADMUX (0x7C) to right-adjust the result
+  // ADCL will contain lower 8 bits, ADCH upper 2 (in last two bits)
+  ADMUX &= B11011111;
+  
+  // Set REFS1..0 in ADMUX (0x7C) to change reference voltage to the
+  // proper source (01)
+  ADMUX |= B01000000;
+  
+  // Clear MUX3..0 in ADMUX (0x7C) in preparation for setting the analog
+  // input
+  ADMUX &= B11110000;
+  
+  // Set MUX3..0 in ADMUX (0x7C) to read from AD8 (Internal temp)
+  // Do not set above 15! You will overrun other parts of ADMUX. A full
+  // list of possible inputs is available in Table 24-4 of the ATMega328
+  // datasheet
+  ADMUX |= 5;
+  // ADMUX |= B00001000; // Binary equivalent
+  
+  // Set ADEN in ADCSRA (0x7A) to enable the ADC.
+  // Note, this instruction takes 12 ADC clocks to execute
+  ADCSRA |= B10000000;
+  
+  // Set ADATE in ADCSRA (0x7A) to enable auto-triggering.
+  ADCSRA |= B00100000;
+  
+  // Clear ADTS2..0 in ADCSRB (0x7B) to set trigger mode to free running.
+  // This means that as soon as an ADC has finished, the next will be
+  // immediately started.
+  ADCSRB &= B11111000;
+  
+  // Set the Prescaler to 128 (16000KHz/128 = 125KHz)
+  // Above 200KHz 10-bit results are not reliable.
+  ADCSRA |= B00000111;
+  
+  // Set ADIE in ADCSRA (0x7A) to enable the ADC interrupt.
+  // Without this, the internal interrupt will not trigger.
+  ADCSRA |= B00001000;
+  
+  // Enable global interrupts
+  // AVR macro included in <avr/interrupts.h>, which the Arduino IDE
+  // supplies by default.
+  sei();
+  
+  // Kick off the first ADC
+  readFlag = 0;
+  // Set ADSC in ADCSRA (0x7A) to start the ADC conversion
+  ADCSRA |=B01000000;
+  Serial.println("ADC timer initialized.");
+}
+
