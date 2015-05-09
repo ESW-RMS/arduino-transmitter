4cb6242b3e02a5c7bb8deea97bf9520628e7d62f
Evan Giarta <egiarta1671@gmail.com>
2015-05-08 17:21:08 -0700

    Timer Interrupts and ADC free running mode are initialized and working on this mode with poll user AT commands working too. Need to make sure GSM sheild is on, need to write setup code that turns on the sheild if off and lets it alone if on.

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -36,7 +36,7 @@
 #define ANALOG_PIN_OFFSET 3
 #define PHASE_NUMBER_OFFSET 13
 #define OUTPUT_PIN 13
-#define SMS_SEND_PERIOD 32                                    // in seconds, this will be 3600 = 1 hour
+#define SMS_SEND_PERIOD 60                                   // in seconds, this will be 3600 = 1 hour
 #define INTERRUPT_PERIOD 4 // highest integer numbers of second a timer interrupt is achievable with 16MHz clock and 1024 pre scale factor
 #define SMS_INTERRUPT_CYCLES SMS_SEND_PERIOD/INTERRUPT_PERIOD // remove this when testing is done
 #define NUM_SMS_COMMANDS 4
@@ -93,17 +93,20 @@ void setup() {
 //  sei();
 }
 
-//void loop() {
-//  pollUserCommand();
-//    
-//  while (shieldGSM.available()) Serial.write(shieldGSM.read());
-//
-//  if(flagAutoSMS) {
-////    sendSMSMessage("message from loop");
-////    Serial.println("message from loop");
-//    flagAutoSMS=false;  
-//  }
-//
+void loop() {
+  pollUserCommand();
+  while (shieldGSM.available()) Serial.write(shieldGSM.read());
+
+  if(flagAutoSMS) {
+    if (readFlag == 1){
+      Serial.println(analogVal);
+      readFlag = 0;
+    }
+    sendSMSMessage("message from loop");
+//    Serial.println("message from loop");
+    flagAutoSMS=false;
+  }
+
 //  for(register int i=0;i<1;i++) {
 //    quantity *q = sensorInputs[0];
 //    unsigned int sensorReading = analogRead(q->port);
@@ -114,36 +117,12 @@ void setup() {
 //      q->min = sensorReading;
 //    }
 //  }
-//}
-
-// Processor loop
-void loop(){
-
-  // Check to see if the value has been updated
-  if (readFlag == 1){
-    
-    Serial.println(analogVal);
-    // Perform whatever updating needed
-    
-    readFlag = 0;
-  }
-  
-  // Whatever else you would normally have running in loop().
-  
 }
 
 // Interrupt service routine for the ADC completion
 ISR(ADC_vect){
-  
-  // Done reading
   readFlag = 1;
-  
-  // Must read low first
   analogVal = ADCL | (ADCH << 8);
-  
-  // Not needed because free-running mode is enabled.
-  // Set ADSC in ADCSRA (0x7A) to start another ADC conversion
-  // ADCSRA |= B01000000;
 }
 
 boolean toggle2;
@@ -364,53 +343,16 @@ void initializeTimerInterrupts() {
 }
 
 void ADCsetup() {  
-  // clear ADLAR in ADMUX (0x7C) to right-adjust the result
-  // ADCL will contain lower 8 bits, ADCH upper 2 (in last two bits)
   ADMUX &= B11011111;
-  
-  // Set REFS1..0 in ADMUX (0x7C) to change reference voltage to the
-  // proper source (01)
   ADMUX |= B01000000;
-  
-  // Clear MUX3..0 in ADMUX (0x7C) in preparation for setting the analog
-  // input
   ADMUX &= B11110000;
-  
-  // Set MUX3..0 in ADMUX (0x7C) to read from AD8 (Internal temp)
-  // Do not set above 15! You will overrun other parts of ADMUX. A full
-  // list of possible inputs is available in Table 24-4 of the ATMega328
-  // datasheet
   ADMUX |= 5;
-  // ADMUX |= B00001000; // Binary equivalent
-  
-  // Set ADEN in ADCSRA (0x7A) to enable the ADC.
-  // Note, this instruction takes 12 ADC clocks to execute
   ADCSRA |= B10000000;
-  
-  // Set ADATE in ADCSRA (0x7A) to enable auto-triggering.
   ADCSRA |= B00100000;
-  
-  // Clear ADTS2..0 in ADCSRB (0x7B) to set trigger mode to free running.
-  // This means that as soon as an ADC has finished, the next will be
-  // immediately started.
   ADCSRB &= B11111000;
-  
-  // Set the Prescaler to 128 (16000KHz/128 = 125KHz)
-  // Above 200KHz 10-bit results are not reliable.
   ADCSRA |= B00000111;
-  
-  // Set ADIE in ADCSRA (0x7A) to enable the ADC interrupt.
-  // Without this, the internal interrupt will not trigger.
   ADCSRA |= B00001000;
-  
-  // Enable global interrupts
-  // AVR macro included in <avr/interrupts.h>, which the Arduino IDE
-  // supplies by default.
-  sei();
-  
-  // Kick off the first ADC
   readFlag = 0;
-  // Set ADSC in ADCSRA (0x7A) to start the ADC conversion
   ADCSRA |=B01000000;
   Serial.println("ADC timer initialized.");
 }
